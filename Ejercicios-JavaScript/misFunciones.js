cee3cadd5e0137b17e0bd0bfe6fee798f5e63b61
gnibeyro <gnibeyro@gmail.com>
2019-05-22 09:49:35 -0300

    Inicio de ejercicios con JS

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -9,3 +9,33 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function saludar () {
+    alert ("Hola mundo!");
+}
+
+function convertir (id_elemento, valor_elemento) {
+
+    if (isNaN(valor_elemento)){
+        alert("El valor de "+id_elemento+" debe ser numerico.");
+        document.getElementById(id_elemeto).value = "";
+    }
+
+    if (!isNaN(valor_elemento)) {
+
+        if (id_elemento == 'metro') {
+            document.getElementById("pulgada").value = valor_elemento * 39.3701;
+            document.getElementById("pie").value = valor_elemento * 3.28084;
+            document.getElementById("yarda").value = valor_elemento * 1.09361;
+        }
+
+        if (id_elemento == 'pulgada') {
+            document.getElementById("metro").value = valor_elemento * 0.0254;
+            document.getElementById("pie").value = valor_elemento * 0.0833;
+            document.getElementById("yarda").value = valor_elemento * 0.0277778;
+        }
+
+        if (id_elemento == 'pie') {
+
+        }
+    }
+}
